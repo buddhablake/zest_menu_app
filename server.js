2337f12bf9f2a9d4d94e8d8c5808241e3baea2df
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-07-16 21:54:35 -0700

    please work

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const mongoose = require("mongoose");
-const db = mongoose.connection;
-require("dotenv").config();
+// const mongoose = require("mongoose");
+// const db = mongoose.connection;
+// require("dotenv").config();
 
 //================
 //MIDDLEWARE
 //================
-// app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-// Serve static files from the React app
+// // app.use(cors());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
 
 //=====================
 //MONGODB/MONGOOSE
 //=====================
 const PORT = process.env.PORT || 5000;
-const MONGODB_URI = process.env.MONGODB_URI;
-
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-});
-
-// Error / success
-db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
-db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
-db.on("disconnected", () => console.log("mongo disconnected"));
+// const MONGODB_URI = process.env.MONGODB_URI;
+//
+// mongoose.connect(MONGODB_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useFindAndModify: false,
+// });
+//
+// // Error / success
+// db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+// db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+// db.on("disconnected", () => console.log("mongo disconnected"));
 
 app.get("/test", (req, res) => {
   res.json({
