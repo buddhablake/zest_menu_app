3c8ccba41500a6b76b630c2c54c5c2c853abe247
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-07-16 21:14:23 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("/test", (req, res) => {
+  res.send("Hello!");
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`listening on ${port}`);
