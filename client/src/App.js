1c19e1e8b0cec0feee149515657887d59a7ba25d
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-07-16 21:42:31 -0700

    Compiling react app one component at a time and and deploying to catch errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,155 @@
 import React, { Component } from "react";
-import "./App.css";
 import axios from "axios";
+import "./App.css";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import MenuItemForm from "./MenuItemForm";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
-  // Initialize state
-  state = { test: "" };
+  state = {
+    menu: null,
+    catOrderId: 0,
+    category: "",
+  };
 
-  // Fetch passwords after first mount
-  componentDidMount() {
-    axios.get("/test").then((res) => {
-      console.log(res);
+  sortMenu = (menu) => {
+    menu.sort((a, b) => {
+      if (a.catOrderId > b.catOrderId) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    const nextCatOrderId = menu[menu.length - 1].catOrderId + 1;
+
+    this.setState({
+      menu,
+      catOrderId: nextCatOrderId,
+    });
+  };
+
+  componentDidMount = () => {
+    axios.get("/menu").then((response) => {
+      if (response.data.length) {
+        this.sortMenu(response.data);
+      }
+    });
+  };
+
+  setCategory = (e) => {
+    this.setState({
+      category: e.target.value,
+    });
+  };
+
+  createCategory = (e) => {
+    e.preventDefault();
+    const { category, catOrderId } = this.state;
+
+    axios.post("/menu", { category, catOrderId }).then((response) => {
+      console.log(response.data);
+      this.sortMenu(response.data);
       this.setState({
-        test: res.data.test,
+        category: "",
       });
     });
-  }
+  };
+
+  setNewCatOrder = (newMenuCatOrder) => {
+    this.setState({
+      menu: newMenuCatOrder,
+    });
+    axios.post("/new-menu", newMenuCatOrder).then((response) => {});
+  };
+
+  setItem = (e) => {
+    this.setState({
+      image: document.querySelector("#itemImage").value,
+      title: e.target.id === "itemTitle" ? e.target.value : this.state.title,
+      description:
+        e.target.id === "itemDescription"
+          ? e.target.value
+          : this.state.description,
+      price: e.target.id === "itemPrice" ? e.target.value : this.state.price,
+      available: e.target.id === "itemAvailability" ? e.target.checked : false,
+      categoryId:
+        e.target.id === "itemCategory"
+          ? e.target.value
+          : document.querySelector("#itemCategory").value,
+    });
+  };
+
+  createItem = (e) => {
+    e.preventDefault();
+    const {
+      image,
+      title,
+      description,
+      price,
+      available,
+      categoryId,
+    } = this.state;
+    const menuItem = {
+      image,
+      title,
+      description,
+      price,
+      available,
+    };
+
+    axios.put("/menu/" + categoryId, menuItem).then((response) => {
+      this.sortMenu(response.data);
+    });
+  };
 
   render() {
-    const { test } = this.state;
+    const { menu, category } = this.state;
+    const {
+      setCategory,
+      createCategory,
+      setItem,
+      createItem,
+      setNewCatOrder,
+    } = this;
+    return (
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Menu</Link>
+            </li>
 
-    return <div>{test}</div>;
+            <li>
+              <Link to="/dash">Dashboard</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="App">
+          <Switch>
+            <Route exact path="/">
+              <Menu menu={menu} />
+            </Route>
+
+            <Route path="/dash">
+              <Categories
+                createCategory={createCategory}
+                setCategory={setCategory}
+                menu={menu}
+                setNewCatOrder={setNewCatOrder}
+              />
+              <MenuItemForm
+                menu={menu}
+                setItem={setItem}
+                createItem={createItem}
+              />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
 
--- server.js
+++ server.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const path = require("path");
-
 const app = express();
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+require("dotenv").config();
 
+//================
+//MIDDLEWARE
+//================
+// app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build")));
+
+//=====================
+//MONGODB/MONGOOSE
+//=====================
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+// Error / success
+db.on("error", (err) => console.log(err.message + " is Mongod not running?"));
+db.on("connected", () => console.log("mongo connected: ", MONGODB_URI));
+db.on("disconnected", () => console.log("mongo disconnected"));
 
 app.get("/test", (req, res) => {
   res.json({
@@ -12,11 +37,78 @@ app.get("/test", (req, res) => {
   });
 });
 
+app.post("/menu", (req, res) => {
+  Menu.create(req.body, (err, newCat) => {
+    if (err) {
+      res.send(err);
+    } else {
+      Menu.find({}, (err, menu) => {
+        res.json(menu);
+      });
+    }
+  });
+});
+
+app.get("/menu/:catId", (req, res) => {
+  Menu.categories.find({ _id: req.params.catId }, (err, category) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(category);
+    }
+  });
+});
+
+app.get("/menu", (req, res) => {
+  Menu.find({}, (err, menu) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(menu);
+    }
+  });
+});
+
+app.post("/new-menu", (req, res) => {
+  let cats = [...req.body];
+  for (let i = 0; i < cats.length; i++) {
+    Menu.findByIdAndUpdate(
+      cats[i]._id,
+      cats[i],
+      { new: true },
+      (err, updatedCat) => {
+        if (err) {
+          log(err);
+        }
+      }
+    );
+  }
+  Menu.find({}, (err, menu) => {
+    res.json(menu);
+  });
+});
+
+app.put("/menu/:id", (req, res) => {
+  Menu.findByIdAndUpdate(
+    req.params.id,
+    { $push: { items: req.body } },
+    { new: true },
+    (err, newItem) => {
+      if (err) {
+        res.send(err);
+      } else {
+        Menu.find({}, (err, menu) => {
+          res.json(menu);
+        });
+      }
+    }
+  );
+});
+
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port);
-
-console.log(`listening on ${port}`);
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
