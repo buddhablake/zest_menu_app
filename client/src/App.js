86039facc6db157d0502f96af4471bfb20ca29fc
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-07-21 15:17:23 -0700

    Refactored app.js by oulling out repeated Nav HTML and placed nav in its own component.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
+import Nav from "./Nav";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import MenuItemForm from "./MenuItemForm";
@@ -153,24 +154,25 @@ class App extends Component {
       <div className="App">
         <Router>
           <div className="p-4">
-            <div className="bg-green-500 min-w-full flex justify-between items-center py-4 px-6 -m-4 mb-6 text-white font-hairline tracking-widest">
-              <h1 className="text-3xl w-1/2">ZEST</h1>
-              <ul className="text-lg font-bold tracking-wider px-6 flex">
-                <li className="px-4">
-                  <Link to="/">Menu</Link>
-                </li>
-                <li>
-                  <Link to="/dash">Dash</Link>
-                </li>
-              </ul>
-            </div>
             <Switch>
+              {/*
+            ===============
+            MENU
+            ===============
+              */}
               <Route exact path="/">
+                <Nav />
                 <Menu menu={menu} />
               </Route>
 
+              {/*
+              ===============
+              DASHBOARD
+              ===============
+                */}
               <Route path="/dash">
                 <div style={{ minWidth: "900px" }}>
+                  <Nav />
                   <div className="grid grid-cols-3 gap-12 w-full min-h-screen bg-gray-300 p-12 rounded-lg">
                     <div className="col-span-1">
                       <Categories
---
+++ client/src/Nav.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+class Nav extends Component {
+  render = () => {
+    return (
+      <div className="bg-green-500 min-w-full flex justify-between items-center py-4 px-6 -m-4 mb-6 text-white font-hairline tracking-widest">
+        <h1 className="text-3xl w-1/2">ZEST</h1>
+        <ul className="text-lg font-bold tracking-wider px-6 flex">
+          <li className="px-4">
+            <Link to="/">Menu</Link>
+          </li>
+          <li>
+            <Link to="/dash">Dash</Link>
+          </li>
+        </ul>
+      </div>
+    );
+  };
+}
+
+export default Nav;
