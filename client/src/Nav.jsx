f0805b14a502453b124bcf2c96074e983e89293e
Blake Douglass <61671758+buddhablake@users.noreply.github.com>
2020-07-29 18:51:26 -0700

    turned zest logo into clickable link that takes users back to the menu

--- client/src/Nav.jsx
+++ client/src/Nav.jsx
@@ -4,7 +4,9 @@ class Nav extends Component {
   render = () => {
     return (
       <div className="bg-green-500 min-w-full flex justify-between items-center py-4 px-6 -m-4 mb-6 text-white font-hairline tracking-widest">
-        <h1 className="text-3xl w-1/2">ZEST</h1>
+        <Link to="/">
+          <h1 className="text-3xl w-1/2">ZEST</h1>
+        </Link>
         <ul className="text-lg font-bold tracking-wider px-6 flex">
           <li className="px-4">
             <Link to="/">Menu</Link>
